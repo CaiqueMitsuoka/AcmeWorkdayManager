0c98176302607e87e95ae198a09fe1d1a9c122e9
Caique Hitoshi Mitsuoka <caique.mitsuoka@gmail.com>
2018-05-19 21:36:57 -0300

    Update Employee with week values

--- lib/acme_workday_manager/models/employee.ex
+++ lib/acme_workday_manager/models/employee.ex
@@ -1,21 +1,41 @@
-defmodule AcmeWorkdayManager.Models do
-  defmodule Employee do
-    defstruct name: nil, pis_number: nil, workload: [], entries: []
+defmodule AcmeWorkdayManager.Models.Employee do
+  alias __MODULE__
+  alias AcmeWorkdayManager.Models.Week
 
-    def new(collection) when is_list(collection) do
-      Enum.map(collection, &new(&1))
-    end
+  defstruct name: nil, pis_number: nil, week_workload: %Week{}, week_rest: %Week{}, entries: []
 
-    def new(%{name: name, pis_number: pis_number, workload: workload}) do
-      %Employee{
-        name: name,
-        pis_number: pis_number,
-        workload: workload
-      }
-    end
+  def new(collection) when is_list(collection) do
+    Enum.map(collection, &new(&1))
+  end
 
-    def find_by_pis_number(employee_enum, pis_number) do
-      Enum.find(employee_enum, &(&1.pis_number == pis_number))
-    end
+  def new(%{name: name, pis_number: pis_number, workload: workload}) do
+    %Employee{
+      name: name,
+      pis_number: pis_number,
+      week_workload: parse_workload(workload),
+      week_rest: parse_rest(workload)
+    }
+  end
+
+  def find_by_pis_number(employee_enum, pis_number) do
+    Enum.find(employee_enum, &(&1.pis_number == pis_number))
+  end
+
+  defp parse_workload(workloads) do
+    parse_week(workloads, :workload_in_minutes)
+  end
+
+  defp parse_rest(workloads) do
+    parse_week(workloads, :minimum_rest_interval_in_minutes)
+  end
+
+  defp parse_week(workloads, attr) do
+    week = Week.new()
+
+    Enum.reduce(workloads, week, fn workload, week ->
+      Enum.reduce(workload.days, week, fn day, week ->
+        Week.update_minutes(week, day, &(&1 + Map.get(workload, attr)))
+      end)
+    end)
   end
 end
--- test/acme_workday_manager/models/employee_test.exs
+++ test/acme_workday_manager/models/employee_test.exs
@@ -2,26 +2,44 @@ defmodule EmployeeTest do
   use ExUnit.Case, async: true
   alias AcmeWorkdayManager.Models.Employee
 
-  setup do
-    %{
-      employees: [
-        %Employee{pis_number: "123", name: "Foo"}
-      ]
-    }
-  end
+  describe "Employee.new/1" do
+    setup do
+      %{
+        employee: %{
+          workload: [
+            %{
+              workload_in_minutes: 90,
+              minimum_rest_interval_in_minutes: 10,
+              days: ["mon", "fri"]
+            }
+          ],
+          pis_number: "123",
+          name: "Foo Bar"
+        }
+      }
+    end
 
-  describe("Employee.find_by_pis_number/2 when employee exist") do
-    setup :pis_number_exist
+    test("it add the week_workload correct", context) do
+      employee = Employee.new(context.employee)
 
-    test "An employee is returned", context do
-      result = AcmeWorkdayManager.Models.Employee.find_by_pis_number(context.employees, "456")
+      assert employee.week_workload.mon == 90
+      assert employee.week_workload.fri == 90
+    end
 
-      assert result.name == "Bar"
+    test("it add the week_rest correct", context) do
+      employee = Employee.new(context.employee)
+
+      assert employee.week_rest.mon == 10
+      assert employee.week_rest.fri == 10
     end
   end
 
+  defp employees_list(_) do
+    %{employees: [%Employee{pis_number: "123", name: "Foo"}]}
+  end
+
   describe("Employee.find_by_pis_number/2 when employee doesnt exist") do
-    setup :pis_number_doesnt_exist
+    setup :employees_list
 
     test "An employee is returned", context do
       result = AcmeWorkdayManager.Models.Employee.find_by_pis_number(context.employees, "456")
@@ -34,7 +52,13 @@ defmodule EmployeeTest do
     %{employees: [%Employee{pis_number: "456", name: "Bar"} | context.employees]}
   end
 
-  defp pis_number_doesnt_exist(context) do
-    context
+  describe("Employee.find_by_pis_number/2 when employee exist") do
+    setup [:employees_list, :pis_number_exist]
+
+    test "An employee is returned", context do
+      result = AcmeWorkdayManager.Models.Employee.find_by_pis_number(context.employees, "456")
+
+      assert result.name == "Bar"
+    end
   end
 end
