377c8af786c1ccba04fcc7d6d134ad3ef10fbe37
Caique Hitoshi Mitsuoka <caique.mitsuoka@gmail.com>
2018-05-22 00:44:00 -0300

    Update AcmeWorkdayManaget with init/2

--- lib/acme_workday_manager.ex
+++ lib/acme_workday_manager.ex
@@ -1,18 +1,16 @@
 defmodule AcmeWorkdayManager do
-  @moduledoc """
-  Documentation for AcmeWorkdayManager.
-  """
+  def init(config, data) do
+    config = AcmeWorkdayManager.Config.open(config)
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> AcmeWorkdayManager.hello
-      :world
-
-  """
-  def hello do
-    :world
+    %{
+      config: config,
+      employees:
+        config.employees
+        |> AcmeWorkdayManager.Models.Employee.new()
+        |> AcmeWorkdayManager.Timeclock.add_entries_to(data)
+        |> Enum.map(&AcmeWorkdayManager.Service.Entries.get_work_report/1)
+        |> Enum.map(&AcmeWorkdayManager.Service.Balance.get_balance/1)
+        |> AcmeWorkdayManager.Service.Summary.get_summary(config.period_start, config.today)
+    }
   end
 end
